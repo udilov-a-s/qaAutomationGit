e8cee16cfc1620e1005af7dfdc680e0a419e8d14
udilov-a-s <67996625+udilov-a-s@users.noreply.github.com>
2023-12-25 23:38:46 +0600

    Update Main.java

--- src/main/java/com/udilovAit/Main.java
+++ src/main/java/com/udilovAit/Main.java
@@ -2,7 +2,7 @@ package com.udilovAit;
 
 public class Main {
 
-    private static String message = "Hello all!!!!";
+    private static String message = "Hello all!";
     
     public static void main(String[] args) {
 
